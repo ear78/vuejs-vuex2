4b337a74656ac58cc580874d853a8c841ec26567
ear78 <ear78@aol.com>
2018-06-11 15:50:51 -0700

    added new home page, deleted hello comp.  Added new menu-li component for nav menu

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,17 @@
 <template>
 
+  <h1>{{ title }}</h1>
 </template>
 
 <script>
 
+  export default {
+    data(){
+      return {
+        title: 'Welcome to the About page...'
+      }
+    }
+  }
 </script>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+
+    <ul class="navigation">
+      <li>Test Li</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        data(){
+          return {
+            title: 'This is the home page...'
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
