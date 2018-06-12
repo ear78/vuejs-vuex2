4a0effb65b8fdbe3f6506a13645a673a76a72ff7
ear78 <ear78@aol.com>
2018-06-12 08:54:37 -0700

    added header component for reusibility

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,39 +2,15 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-
-    <!-- dynamically building menu using single li component passing props from parent to child -->
-    <ul class="navigation">
-      <NavItem v-bind:links="i" v-for="i in menu"></NavItem>
-    </ul>
   </div>
 </template>
 
 <script>
-  import NavItem from './ui-components/NavItem';
-
     export default {
         name: 'Home',
-        components: {
-          NavItem: NavItem
-        },
         data(){
           return {
-            title: 'This is the home page...',
-            menu: [
-              {
-                path: '/',
-                link: 'Home'
-              },
-              {
-                path: '/about',
-                link: 'About'
-              },
-              {
-                path: '/contact',
-                link: 'Contact'
-              }
-            ]
+            title: 'This is the home page...'
           }
         }
     }
---
+++ src/components/ui-components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- dynamically building menu using single li component passing props from parent to child -->
+  <ul class="navigation">
+    <NavItem v-bind:links="i" v-for="i in menu"></NavItem>
+  </ul>
+</template>
+
+<script>
+  import NavItem from './NavItem';
+
+  export default {
+    name: 'Header',
+    components: {
+      NavItem: NavItem
+    },
+    data(){
+      return {
+        title: 'This is the home page...',
+        menu: [
+          {
+            path: '/',
+            link: 'Home'
+          },
+          {
+            path: '/about',
+            link: 'About'
+          },
+          {
+            path: '/contact',
+            link: 'Contact'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+
+  .active {
+    color: blue;
+  }
+</style>
